783ce5ac86e53053b0b5f343b259c5c1dfd4ed25
Searhey Volochanovich <johndoe@example.comsergey.04.10.1991@gmail.com>
2020-07-28 13:44:39 +0300

    task 17:done

---
+++ 1_numbers/17/17.php
@@ -0,0 +1,47 @@
+<?php
+	function getGCD(int $number1, int $number2): int {
+		$number1 = getAbsolute($number1);
+		$number2 = getAbsolute($number2);
+		$start = 0;
+		
+		if (!($number1 > 0 && $number2 > 0)) {
+			return 0;
+		}
+		
+		$fewer = $number1 < $number2 ? $number1 : $number2;
+		
+		for ($divider = $fewer; $divider > 1; $divider--) {
+			
+			
+			if (isDivider($number1, $divider) && isDivider($number2, $divider)) {
+				return $divider;
+			}
+			
+		}
+		
+		return 1;
+	}	
+		
+	function isDivider(int $number, int $divider): bool {
+		$remainder = $number % $divider;
+		
+		if ($remainder === 0) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	function getAbsolute(int $number): int {
+		
+		if ($number < 0) {
+			return $number * -1;
+		}
+		
+		return $number;
+	}
+	
+	//пример
+	echo getGCD(248, 472);
+	
+?>
